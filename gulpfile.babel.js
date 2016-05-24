b06d3fd635e304ee073061b6de994afa7f626b83
Tirell M <tdmckinn@gmail.com>
2016-05-23 20:23:59 -0400

    bump packages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,18 +3,18 @@ import gulp from 'gulp';
 import schema from 'gulp-graphql';
 
 // Regenerate the graphql schema and recompile the frontend code that relies on schema.json
-gulp.task('generate-schema', () => {
-  return gulp.src('./server/data/schema.js')
+gulp.task('generate-schema', () =>
+ gulp.src('./server/data/schema.js')
     .pipe(schema({
       json: true,
       graphql: false
     }))
     .on('error', console.log)
-    .pipe(gulp.dest('./server/data'));
-});
+    .pipe(gulp.dest('./server/data'))
+);
 
 gulp.task('watch-schema', () => {
-  gulp.watch('./server/data/schema.js', [ 'generate-schema' ]);
+  gulp.watch('./server/data/schema.js', ['generate-schema']);
 });
 
 gulp.task('default', ['schema']);
