dc8fad323001dc18c13961448bdd2a67f51397e9
Tirell M <tdmckinn@gmail.com>
2016-05-24 19:03:35 -0400

    Update schema generation tool

--- babelRelayPlugin.js
+++ babelRelayPlugin.js
@@ -1,6 +1,6 @@
 
 /** Required per docs if not using the starter kit
- * https://facebook.github.io/relay/docs/guides-babel-plugin.html
+ *  https://facebook.github.io/relay/docs/guides-babel-plugin.html
  **/
 
 let getbabelRelayPlugin = require('babel-relay-plugin');
